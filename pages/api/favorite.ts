40873e1b7bd208df8eacce65b7726bfe72393fe5
IdanKfir <zunzer244@gmail.com>
2023-05-17 14:46:05 +0300

    package.json changes

--- pages/api/favorite.ts
+++ pages/api/favorite.ts
@@ -13,28 +13,28 @@ export default async function handler(
       const { currentUser } = await serverAuth(req, res);
 
       const { movieId } = req.body;
-  
+
       const existingMovie = await prismadb.movie.findUnique({
         where: {
           id: movieId,
-        }
+        },
       });
-  
+
       if (!existingMovie) {
         throw new Error('Invalid ID');
       }
-  
+
       const user = await prismadb.user.update({
         where: {
           email: currentUser.email || '',
         },
         data: {
           favoriteIds: {
-            push: movieId
-          }
-        }
+            push: movieId,
+          },
+        },
       });
-  
+
       return res.status(200).json(user);
     }
 
